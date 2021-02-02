c3bb5e7ad172679d6005deb3b65ab4eb5874b57c
nabilmasudh <73231267+nabilmasudh@users.noreply.github.com>
2021-02-02 20:06:18 +0600

    Add files via upload
    https://projecthtmlandcss.netlify.app

---
+++ project html and css/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function () {
+	jQuery('#main_menu').meanmenu({
+		meanMenuContainer: '#mobile_menu',
+		meanScreenWidth: 991
+	});
+});
+
+$(document).ready(function(){
+    $('.slider_area').bxSlider();
+  });
